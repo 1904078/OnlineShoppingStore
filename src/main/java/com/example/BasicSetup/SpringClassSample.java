92f00f5b40feb97eb78e84578e08a6301f56ffb6
SinghJitender <Jitu.singh95@gmail.com>
2019-12-27 20:29:51 +0530

    Logging Enabled

--- src/main/java/com/example/BasicSetup/SpringClassSample.java
+++ src/main/java/com/example/BasicSetup/SpringClassSample.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringClassSample {
-  @GetMapping("/")
+  @GetMapping("/helloWorld")
   public String helloWorld() {
     return "Hello to SPRING!!";
   }
