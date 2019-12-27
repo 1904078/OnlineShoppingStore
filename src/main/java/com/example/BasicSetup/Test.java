afcf425ffc9f2b698b61fc59c5f0aa58fd6ae840
SinghJitender <Jitu.singh95@gmail.com>
2019-12-27 19:20:04 +0530

    Spring Setup - Working

---
+++ src/main/java/com/example/BasicSetup/Test.java
@@ -0,0 +1,12 @@
+package com.example.BasicSetup;
+
+public class Test {
+  /**
+  * This is main method to test application.
+  */
+  /*public static void main(String[] args) {
+    Logger log = LoggerFactory.getLogger(com.example.BasicSetup.Test.class);
+    log.debug("It's wokring fine till line 8");
+    System.out.println("com.example.BasicSetup.Test Successful");
+  }*/
+}
