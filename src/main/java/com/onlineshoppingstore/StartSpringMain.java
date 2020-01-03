6649eeef4c410a11cfbdd8bb847ddfb0268085ee
Jitender Pal Singh <Jitu.singh95@gmail.com>
2020-01-04 00:42:38 +0530

    Update and rename StartSpringTest.java to StartSpringMain.java

--- src/main/java/com/onlineshoppingstore/StartSpringMain.java
+++ src/main/java/com/onlineshoppingstore/StartSpringMain.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class StartSpringTest {
+public class StartSpringMain {
   private static final Logger logger = LoggerFactory.getLogger(StartSpringTest.class);
 
   public static void main(String[] args) {
